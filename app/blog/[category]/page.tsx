18228a1be1849db6786028a12da8c42cbdde6196
Alex Verdaguer <alex@wualia.com>
2025-07-08 17:49:15 +0200

    sdfds

--- app/blog/[category]/page.tsx
+++ app/blog/[category]/page.tsx
@@ -34,11 +34,11 @@ export async function generateMetadata(
     description: `Blog de Solvify, noticias y artículos sobre ${categoryData.docs[0].name}`,
     metadataBase: new URL(`${process.env.SITE_URL}`),
     alternates: {
-      canonical: `/blog/${post.docs[0].categorySlug}`,
+      canonical: `/blog/${post.docs[0]?.categorySlug}`,
     },
     openGraph: {
-      title: `Blog ${post.docs[0].categorySlug} | Solvify`,
-      description: `Blog de Solvify, noticias y artículos sobre ${post.docs[0].categorySlug}`,
+      title: `Blog ${post.docs[0]?.categorySlug} | Solvify`,
+      description: `Blog de Solvify, noticias y artículos sobre ${post.docs[0]?.categorySlug}`,
       siteName: "Solvify",
       type: "website",
     },
@@ -57,10 +57,7 @@ const BlogCategoryPage = async ({
   );
   const posts = await data.json();
 
-  const categoriesData = await fetch(
-    `${process.env.BLOG_URL}/api/categories?sort=order`
-  );
-  const categories = await categoriesData.json();
+  console.log("posts", posts);
 
   return (
     <div className="mx-auto max-w-7xl lg:py-8 grid lg:grid-cols-4 gap-4 items-start">
@@ -71,30 +68,44 @@ const BlogCategoryPage = async ({
         <MobileCategories />
       </div>
       <div className="lg:col-span-3 space-y-4 px-4 2xl:px-0">
-        {posts.docs?.map((post: any) => (
-          <div key={post.id} className="border-b last-of-type:border-b-0 py-4">
-            <Link href={`/blog/${post.categorySlug}/${post.slug}`} className="">
-              <div>
-                <Badge variant="outline" className="text-sm">
-                  {post.category.name}
-                </Badge>
-                <h2 className="md:text-2xl font-medium py-4 text-gray-700 dark:text-white">
-                  {post.title}
-                </h2>
-                <p className="text-muted-foreground max-w-3xl">
-                  {post.excerpt}
-                </p>
-              </div>
-              <div className="flex items-center gap-4 py-4">
-                <p className="text-sm text-primary">
-                  {format(new Date(post.updatedAt), "dd MMMM yyyy", {
-                    locale: es,
-                  })}
-                </p>
-              </div>
-            </Link>
-          </div>
-        ))}
+        <>
+          {posts.docs.length === 0 ? (
+            <></>
+          ) : (
+            <>
+              {posts.docs?.map((post: any) => (
+                <div
+                  key={post.id}
+                  className="border-b last-of-type:border-b-0 py-4"
+                >
+                  <Link
+                    href={`/blog/${post.categorySlug}/${post.slug}`}
+                    className=""
+                  >
+                    <div>
+                      <Badge variant="outline" className="text-sm">
+                        {post.category.name}
+                      </Badge>
+                      <h2 className="md:text-2xl font-medium py-4 text-gray-700 dark:text-white">
+                        {post.title}
+                      </h2>
+                      <p className="text-muted-foreground max-w-3xl">
+                        {post.excerpt}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-4 py-4">
+                      <p className="text-sm text-primary">
+                        {format(new Date(post.updatedAt), "dd MMMM yyyy", {
+                          locale: es,
+                        })}
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              ))}
+            </>
+          )}
+        </>
       </div>
     </div>
   );
