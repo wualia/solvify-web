715148cc851d36070e91ebbd00cfb273dd029fac
Alex Verdaguer <alex@wualia.com>
2025-07-08 16:41:08 +0200

    sfds

--- components/layout/mobile.tsx
+++ components/layout/mobile.tsx
@@ -33,64 +33,69 @@ const Mobile = () => {
           className="bg-black z-50 fixed inset-0 w-full h-full flex flex-col"
         >
           <div className="flex justify-between items-center mb-8 border-b border-white/20 px-4 py-5">
-            <p className="text-white font-medium uppercase text-sm">Menú</p>
+            <p className="text-gray-300 font-medium uppercase text-sm">Menú</p>
             <div onClick={() => setWebMobileOpen()} className="text-white">
-              <CircleXIcon className="w-6 h-6" />
+              <CircleXIcon className="w-6 h-6 text-gray-300" />
             </div>
           </div>
           <ul className="text-white flex-1 px-4">
-            <li onClick={() => goTo("/")} className="pb-8 text-xl font-medium">
+            <li
+              onClick={() => goTo("/")}
+              className="pb-8 text-xl font-medium tracking-wider"
+            >
               Inicio
             </li>
-            <li className=" pb-4 text-xl font-medium">Servicios</li>
+            <li className=" pb-4 text-xl font-medium tracking-wider">
+              Servicios
+            </li>
             <ul className="pb-8 text-gray-300">
               <li
                 onClick={() => goTo("/servicios/ley-de-segunda-oportunidad")}
-                className="border-b border-white/20 py-4"
+                className="border-b border-white/10 py-4"
               >
                 Ley de segunda oportunidad
               </li>
               <li
                 onClick={() => goTo("/servicios/negociacion-de-deudas")}
-                className="border-b border-white/20 py-4"
+                className="border-b border-white/10 py-4"
               >
                 Negociación de deuda
               </li>
               <li
                 onClick={() => goTo("/servicios/tarjetas-revolving")}
-                className="border-b border-white/20 py-4"
+                className="border-b border-white/10 py-4"
               >
                 Tarjetas revolving
               </li>
               <li
                 onClick={() => goTo("/servicios/cartel-coches")}
-                className="border-b border-white/20 py-4"
+                className="border-b border-white/10 py-4"
               >
                 Cártel de coches
               </li>
               <li
                 onClick={() => goTo("/servicios/concurso-expres")}
-                className="border-b border-white/20 py-4"
+                className="border-b border-white/10 py-4"
               >
                 Concurso exprés
               </li>
             </ul>
             <li
               onClick={() => goTo("/blog")}
-              className="pb-8 text-xl font-medium"
+              className="pb-8 text-xl font-medium tracking-wider"
             >
               Blog
             </li>
             <li
               onClick={() => goTo("/contacto")}
-              className="pb-8 text-xl font-medium"
+              className="pb-8 text-xl font-medium tracking-wider"
             >
               Contacto
             </li>
           </ul>
           <div className="px-4 mb-8">
             <Button
-              onClick={() => setWebMobileOpen()}
+              onClick={() => router.push("https://app.solvify.es")}
               className="w-full py-5 bg-white text-black"
             >
               Zona clientes
