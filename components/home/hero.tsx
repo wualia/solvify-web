0cd6c09753bd49fb2a643b00f8d285ce69b229f3
Alex Verdaguer <alex@wualia.com>
2025-07-12 09:58:29 +0200

    asdsa

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -7,7 +7,7 @@ const HeroHome = () => {
     <div className="py-8">
       <div className="mx-auto max-w-7xl px-4 2xl:px-0">
         <div className="rounded-lg overflow-hidden shadow-lg relative">
-          <div className="absolute inset-0 z-20 h-full w-full flex flex-col justify-center px-8 md:px-16">
+          <div className="absolute inset-0 z-40 h-full w-full flex flex-col justify-center px-8 md:px-16">
             <h1 className="text-3xl md:text-5xl font-medium pb-6 text-white text-center md:text-left max-w-3xl leading-tight">
               Solventamos tus problemas económicos
             </h1>
@@ -20,14 +20,16 @@ const HeroHome = () => {
               <HeroButton />
             </div>
           </div>
-          <div className="absolute inset-0 bg-black/30 z-10" />
-          <Image
-            src="/hero-home.webp"
-            width={1920}
-            height={1080}
-            alt="Imagen contacto Solvify"
-            className="object-cover h-[400px] lg:h-[500px] w-full z-20 inset-0 object-top"
-          />
+          <div className="absolute inset-0 bg-black/30 z-30" />
+          <div className="relative h-[400px] lg:h-[500px] w-full z-20 inset-0">
+            <Image
+              src="/hero-home.webp"
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              alt="Imagen contacto Solvify"
+              className="object-cover  object-top"
+            />
+          </div>
         </div>
       </div>
     </div>
